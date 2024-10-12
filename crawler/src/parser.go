78d6e834f8ca32c47e7f16c56ca45b129dfbc6e8
Wojciech Jedynak <wjedynak@gmail.com>
2024-10-12 15:40:46 +0200

    Fix tests

--- crawler/src/parser.go
+++ crawler/src/parser.go
@@ -32,11 +32,30 @@ func parseTitle(title string) (parsedTitle Title, err error) {
 	return
 }
 
+func parseVolumeCount(text string) (volumeCount int, err error) {
+	regex := *regexp.MustCompile(`\s*(\d+) Volumes.+`)
+	results := regex.FindStringSubmatch(text)
+	if len(results) < 2 {
+		return volumeCount, errors.New("volume parse error")
+	}
+
+	volumeCount, err = strconv.Atoi(results[1])
+
+	if err != nil {
+		return volumeCount, errors.New("title parse error")
+	}
+	return
+}
+
 func parse(reader io.Reader) Title {
 	doc, err := goquery.NewDocumentFromReader(reader)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// volumeCountText := doc.Find("#collection-masthead__size").First().Text()
+	// fmt.Println(volumeCountText)
+	// volumeCount, err := parseVolumeCount(volumeCountText)
 
 	title := doc.Find("title").First().Text()
 	fmt.Println(title)
--- crawler/src/parser_test.go
+++ crawler/src/parser_test.go
@@ -6,6 +6,18 @@ import (
 	"testing"
 )
 
+func assertTitle(t testing.TB, want, got Title) {
+	t.Helper()
+
+	if got.title != want.title {
+		t.Errorf("got %q want %q\n", got.title, want.title)
+	}
+
+	if got.volumeCount != want.volumeCount {
+		t.Errorf("got %d want %d\n", got.volumeCount, want.volumeCount)
+	}
+}
+
 func TestTitleParser(t *testing.T) {
 	t.Run("empty string returns error", func(t *testing.T) {
 		_, err := parseTitle("")
@@ -42,24 +54,50 @@ func TestTitleParser(t *testing.T) {
 	})
 }
 
-func assertTitle(t testing.TB, want, got Title) {
-	t.Helper()
+func TestVolumeCountParser(t *testing.T) {
+	t.Run("empty string returns error", func(t *testing.T) {
+		_, err := parseVolumeCount("")
 
-	if got.title != want.title {
-		t.Errorf("got %q want %q\n", got.title, want.title)
-	}
+		got := err.Error()
+		want := errors.New("volume parse error").Error()
 
-	if got.volumeCount != want.volumeCount {
-		t.Errorf("got %d want %d\n", got.volumeCount, want.volumeCount)
-	}
+		if got != want {
+			t.Errorf("got %q want %q\n", got, want)
+		}
+	})
+
+	t.Run("volume count can be parsed", func(t *testing.T) {
+		got, err := parseVolumeCount("            44 Volumes | 1 Episode")
+		want := 44
+
+		if err != nil {
+			t.Errorf("got unexpected error %q\n", err)
+		}
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
 }
 
 func TestParsing(t *testing.T) {
-	dat, err := os.Open("examples/shima.html")
-	Check(err)
+	t.Run("shima kousaku", func(t *testing.T) {
+		dat, err := os.Open("examples/shima.html")
+		Check(err)
 
-	got := parse(dat)
-	want := Title{title: "社外取締役　島耕作", volumeCount: 5}
+		got := parse(dat)
+		want := Title{title: "社外取締役　島耕作", volumeCount: 5}
 
-	assertTitle(t, want, got)
+		assertTitle(t, want, got)
+	})
+
+	// t.Run("uchuu kyoudai", func(t *testing.T) {
+	// 	dat, err := os.Open("examples/uchuu_kyoudai.html")
+	// 	Check(err)
+
+	// 	got := parse(dat)
+	// 	want := Title{title: "宇宙兄弟", volumeCount: 5}
+
+	// 	assertTitle(t, want, got)
+	// })
 }
