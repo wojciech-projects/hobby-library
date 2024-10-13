8eae41453868ab0b9dd4e843f942726c51893d28
Wojciech Jedynak <wjedynak@gmail.com>
2024-10-13 06:59:35 +0200

    refactor

--- crawler/src/parser.go
+++ crawler/src/parser.go
@@ -1,51 +1,12 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"io"
 	"log"
-	"regexp"
-	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
 )
-
-type Title struct {
-	title       string
-	volumeCount int
-}
-
-func parseTitle(title string) (parsedTitle Title, err error) {
-	regex := *regexp.MustCompile(`(.+) \((\d+) book series\).*`)
-	results := regex.FindStringSubmatch(title)
-	if len(results) < 3 {
-		return parsedTitle, errors.New("title parse error")
-	}
-
-	parsedTitle.title = results[1]
-	parsedTitle.volumeCount, err = strconv.Atoi(results[2])
-
-	if err != nil {
-		return parsedTitle, errors.New("title parse error")
-	}
-	return
-}
-
-func parseVolumeCount(text string) (volumeCount int, err error) {
-	regex := *regexp.MustCompile(`\s*(\d+) Volumes.+`)
-	results := regex.FindStringSubmatch(text)
-	if len(results) < 2 {
-		return volumeCount, errors.New("volume parse error")
-	}
-
-	volumeCount, err = strconv.Atoi(results[1])
-
-	if err != nil {
-		return volumeCount, errors.New("title parse error")
-	}
-	return
-}
 
 func parse(reader io.Reader) Title {
 	doc, err := goquery.NewDocumentFromReader(reader)
--- crawler/src/parser_test.go
+++ crawler/src/parser_test.go
@@ -1,84 +1,9 @@
 package main
 
 import (
-	"errors"
 	"os"
 	"testing"
 )
-
-func assertTitle(t testing.TB, want, got Title) {
-	t.Helper()
-
-	if got.title != want.title {
-		t.Errorf("got %q want %q\n", got.title, want.title)
-	}
-
-	if got.volumeCount != want.volumeCount {
-		t.Errorf("got %d want %d\n", got.volumeCount, want.volumeCount)
-	}
-}
-
-func TestTitleParser(t *testing.T) {
-	t.Run("empty string returns error", func(t *testing.T) {
-		_, err := parseTitle("")
-
-		got := err.Error()
-		want := errors.New("title parse error").Error()
-
-		if got != want {
-			t.Errorf("got %q want %q\n", got, want)
-		}
-	})
-
-	t.Run("title without count returns error", func(t *testing.T) {
-		_, err := parseTitle("社外取締役　島耕作 Kindle Edition")
-
-		got := err.Error()
-		want := errors.New("title parse error").Error()
-
-		if got != want {
-			t.Errorf("got %q want %q\n", got, want)
-		}
-	})
-
-	t.Run("title with count can be parsed", func(t *testing.T) {
-		got, err := parseTitle("社外取締役　島耕作 (5 book series) Kindle Edition")
-
-		want := Title{title: "社外取締役　島耕作", volumeCount: 5}
-
-		if err != nil {
-			t.Errorf("got unexpected error %q\n", err)
-		}
-
-		assertTitle(t, got, want)
-	})
-}
-
-func TestVolumeCountParser(t *testing.T) {
-	t.Run("empty string returns error", func(t *testing.T) {
-		_, err := parseVolumeCount("")
-
-		got := err.Error()
-		want := errors.New("volume parse error").Error()
-
-		if got != want {
-			t.Errorf("got %q want %q\n", got, want)
-		}
-	})
-
-	t.Run("volume count can be parsed", func(t *testing.T) {
-		got, err := parseVolumeCount("            44 Volumes | 1 Episode")
-		want := 44
-
-		if err != nil {
-			t.Errorf("got unexpected error %q\n", err)
-		}
-
-		if got != want {
-			t.Errorf("got %d want %d", got, want)
-		}
-	})
-}
 
 func TestParsing(t *testing.T) {
 	t.Run("shima kousaku", func(t *testing.T) {
